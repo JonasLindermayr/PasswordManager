366318f7347686b080375e4b0a626b79b026a50b
Jonas Lindermayr <Lindermayr.jonas@gmx.de>
2025-05-07 20:45:53 +0200

    Implemented mainMenu.go - added bubbletea + lipgloss as dependency

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/JonasLindermayr/PasswordManager/ui"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+var version = "0.0.1"
+
 func main() {
-	// TODO: implement main
+	// TODO: Check if db exists -> if not create one with master password
+	// TODO: Check if db exists -> if check master password is correct
+
+	p := tea.NewProgram(ui.InitialModel(version))
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
 
